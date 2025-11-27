10da4e797f3b380ba02d0996f6bc73579b69bcc6
Micael Rauan <87452033+micaelrauan@users.noreply.github.com>
2025-11-27 08:34:51 -0300

    Atualização

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  {
+    res.redirect("/portfolio");
+  }
+});
+
+app.get("/portfolio", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
